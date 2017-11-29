2c894d22a62e6b5fd12039a90b9a2a08f3ba7966
bitnahian <nhas9102@uni.sydney.edu.au>
2017-11-29 17:44:12 +1100

    Pagination for job search works

--- app/app.py
+++ app/app.py
@@ -14,32 +14,34 @@ def form():
 
 @app.route('/process')
 def process():
-    print("HERE")
     # Construct API params
-    payload = {'api_key' : 'e13265054e9ac26df0699a76972404b7', 'method' : 'aj.jobs.search', 'perpage' : '10'}
+    payload = {'api_key' : 'e13265054e9ac26df0699a76972404b7', 'method' : 'aj.jobs.search', 'perpage' : '4'}
     # Receive keywords from jQuery script
     keywords = request.args.get('keywords', 0, type=str)
+    page = request.args.get('page', 1, type=int)
     if keywords is None:
         return jsonify({"error" : "Keywords not specified"})
 
     keywords.replace(" ", "")
     payload['keywords'] = keywords
+    payload['page'] = page
 
     url='https://authenticjobs.com/api/'
 
     r = requests.get(url, params=payload)
     root = ET.fromstring(r.content)
-    response = {'title' : [], 'description' : []}
+    response = {'title' : [], 'description' : [], 'url' : []}
     for listing in root.iter('listing'):
         title = listing.get('title')
         description = listing.get('description')
+        url = listing.find('company').get('url')
         #print(' * {} {}'.format(
         #    title, description
         #    ))
         #print('\n\n')
         response['title'].append(title)
         response['description'].append(description)
-
+        response['url'].append(url)
     return jsonify(response)
 
 
--- app/static/scripts/form.js
+++ app/static/scripts/form.js
@@ -1,16 +1,80 @@
 $(document).ready(function() {
 
-	$('form').on('submit', function(event) {
+		$('form').on('submit', function(event) {
     $.getJSON('/process', {
 				  keywords: $('#input').val(),
+					page: 1
 				}, function(data) {
-					var myJSON = JSON.stringify(data);
           if (data.error) {
-    				$('#result').html(myJSON.error);
+    				$('#result').html(data.error);
     			} else {
-    				$('#result').html(myJSON);
+						$('#next').show();
+						$('#prev').show();
+						var html = ''
+						for(var i = 0; i < data.title.length; ++i){
+							  //html +=  "<h1><a href='"+ data.url[i] "'/>" + data.title[i] + "</h1> " +  data.description[i] + "</br>";
+								html += '<h1> <a href = "{0}"> {1} </a> </h1> {2} <hr>'.format(data.url[i], data.title[i], data.description[i]);
+						}
+    				$('#result').html(html);
+						$('#page').html(1);
     			}
 				});
         return false;
       });
+
+			$('#next').on('click', function(event) {
+				var pageStr = $('#page').text();
+				var page = parseInt(pageStr) + 1;
+				console.log(page);
+		    $.getJSON('/process', {
+						  keywords: $('#input').val(),
+							page : page
+						}, function(data) {
+		          if (data.error) {
+		    				$('#result').html(data.error);
+		    			} else {
+								var html = ''
+								for(var i = 0; i < data.title.length; ++i){
+								    //html +=  "<h1><a href='"+ data.url[i] "'>" + data.title[i] + "</h1> " +  data.description[i] + "</br>";
+										html += '<h1> <a href = "{0}"> {1} </a> </h1> {2} <hr>'.format(data.url[i], data.title[i], data.description[i]);
+								}
+		    			$('#result').html(html);
+							$('#page').html(page);
+		    		}
+					});
+		      return false;
+				});
+
+				$('#prev').on('click', function(event) {
+					var pageStr = $('#page').text();
+					var page = parseInt(pageStr) - 1;
+					console.log(page);
+			    $.getJSON('/process', {
+							  keywords: $('#input').val(),
+								page : page
+							}, function(data) {
+			          if (data.error) {
+			    				$('#result').html(data.error);
+			    			} else {
+									var html = ''
+									for(var i = 0; i < data.title.length; ++i){
+									    //html +=  "<h1><a href='"+ data.url[i] "'>" + data.title[i] + "</h1> " +  data.description[i] + "</br>";
+											html += '<h1> <a href = "{0}"> {1} </a> </h1> {2} <hr>'.format(data.url[i], data.title[i], data.description[i]);
+									}
+			    				$('#result').html(html);
+									$('#page').html(page);
+			    			}
+							});
+			        return false;
+						});
+
 });
+
+String.prototype.format = function () {
+  var args = arguments;
+  return this.replace(/\{\{|\}\}|\{(\d+)\}/g, function (m, n) {
+    if (m == "{{") { return "{"; }
+    if (m == "}}") { return "}"; }
+    return args[n];
+  });
+};
