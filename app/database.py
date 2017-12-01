d6f01a853460535bfb7118b7f1314462cda1c656
ashrafsrv <ashrafsrv7@gmail.com>
2017-12-02 02:48:46 +1100

    Setting up database

---
+++ app/database.py
@@ -0,0 +1,13 @@
+import sqlite3
+from flask import Flask, render_template, request
+
+try:
+    conn = sqlite3.connect('database.db')
+    print("Database successfully opened")
+    conn.execute('''CREATE TABLE IF NOT EXISTS Jobs(
+                job_id INTEGER,
+                username VARCHAR
+                );''')
+    print("Table successfully created")
+except:
+    print("Failed to open database")
